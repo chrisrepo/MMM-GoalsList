b9a82ce2418fed562df32a8d9511209bd7f84262
Chris Repanich <chrisrepanich@gmail.com>
2018-03-31 15:16:30 -0700

    Initial commit

---
+++ MMM-GoalsList.js
@@ -0,0 +1,25 @@
+/* Magic Mirror
+ * Module: GoalsList
+ *
+ * By Chris Repanich
+ * MIT Licensed.
+ */
+
+Module.register("MMM-GoalsList", {
+	requiresVersion: "2.1.0",
+	
+	//Default module config
+	defaults: {
+		
+	},
+	
+	getDom: function() {
+		var wrapper = document.createElement("div");
+		if (this.addresses.length === 0) {
+			this.addresses = ["ip-of-your-mirror"];
+		}
+		wrapper.innerHTML = "http://" + this.addresses[0] + ":8080/goals.html";
+		wrapper.className = "normal xsmall";
+		return wrapper;
+	}
+});
